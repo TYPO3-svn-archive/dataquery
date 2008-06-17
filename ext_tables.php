0a36b4515cbbac16e19075ffc4d43fe6357556e4
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-17 12:32:40 +0000

    Added registration of dataquery as a Data Provider Restored strangely missing icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dataquery/trunk@10108 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,12 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+// Define main TCA for table tx_dataquery_queries
+
 t3lib_extMgm::allowTableOnStandardPages('tx_dataquery_queries');
 
-$TCA["tx_dataquery_queries"] = array (
-	"ctrl" => array (
+$TCA['tx_dataquery_queries'] = array (
+	'ctrl' => array (
 		'title'     => 'LLL:EXT:dataquery/locallang_db.xml:tx_dataquery_queries',		
 		'label'     => 'title',	
 		'tstamp'    => 'tstamp',
@@ -12,7 +14,7 @@ $TCA["tx_dataquery_queries"] = array (
 		'cruser_id' => 'cruser_id',
 		'versioningWS' => TRUE, 
 		'origUid' => 't3_origuid',
-		'default_sortby' => "ORDER BY title",	
+		'default_sortby' => 'ORDER BY title',	
 		'delete' => 'deleted',	
 		'enablecolumns' => array (		
 			'disabled' => 'hidden',
@@ -20,8 +22,14 @@ $TCA["tx_dataquery_queries"] = array (
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
 		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_dataquery_queries.gif',
 	),
-	"feInterface" => array (
-		"fe_admin_fieldList" => "hidden, title, description, sql_query, t3_mechanisms",
+	'feInterface' => array (
+		'fe_admin_fieldList' => 'hidden, title, description, sql_query, t3_mechanisms',
 	)
 );
+
+// Register dataquery as a Data Provider
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['columns']['tx_displaycontroller_provider']['config']['allowed'] .= ',tx_dataquery_queries';
+
 ?>
