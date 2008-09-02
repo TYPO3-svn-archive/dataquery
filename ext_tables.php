01bc4419f7de4d0fb01702e451530724c8daa8aa
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-02 08:17:45 +0000

    Added "add" wizard for display controller
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dataquery/trunk@11633 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -22,7 +22,7 @@ $TCA['tx_dataquery_queries'] = array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_dataquery_queries.gif',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'res/icons/icon_tx_dataquery_queries.gif',
 	),
 	'feInterface' => array (
 		'fe_admin_fieldList' => 'hidden, title, description, sql_query, t3_mechanisms',
@@ -34,4 +34,18 @@ $TCA['tx_dataquery_queries'] = array (
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['columns']['tx_displaycontroller_provider']['config']['allowed'] .= ',tx_dataquery_queries';
 
+// Add a wizard for adding a dataquery
+
+$addDataqueryWizard = array(
+						'type' => 'script',
+						'title' => 'LLL:EXT:dataquery/locallang_db.xml:wizards.add_dataquery',
+						'script' => 'wizard_add.php',
+						'icon' => 'EXT:dataquery/res/icons/add_dataquery_wizard.gif',
+						'params' => array(
+								'table' => 'tx_dataquery_queries',
+								'pid' => '###CURRENT_PID###',
+								'setValue' => 'append'
+							)
+						);
+$TCA['tt_content']['columns']['tx_displaycontroller_provider']['config']['wizards']['add_dataquery'] = $addDataqueryWizard;
 ?>
