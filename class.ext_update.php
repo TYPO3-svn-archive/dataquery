fc33a255c03b727cc41a8e52f76d3a63c8801b00
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-26 12:58:47 +0000

    Removed commented out code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dataquery/trunk@33502 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.ext_update.php
+++ class.ext_update.php
@@ -92,60 +92,6 @@ class ext_update {
 		} else {
 			$content .= '<p>The old additional SQL field does not exist, there\'s nothing to update.</p>';
 		}
-/*
-		// Get a list of all secondary providers (i.e. providers with a sorting of 2)
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tx_displaycontroller_providers_mm', "sorting = '2'");
-		if ($res) {
-				// No secondary provider exists
-			if ($GLOBALS['TYPO3_DB']->sql_num_rows($res) == 0) {
-				$content .= '<p>There are no relationships to update.</p>';
-			}
-				// There are secondary providers
-			else {
-				$update = t3lib_div::_GP('submitButton');
-					// The update button was not clicked, display information message
-				if (empty($update)) {
-					$content .= '<p>There are '.$GLOBALS['TYPO3_DB']->sql_num_rows($res).' relationships to update. Click on the update button below to start the process.</p>';
-					$content .= '<form name="updateForm" action="" method ="post">';
-					$content .= '<p><input type="submit" name="submitButton" value ="Update"></p>';
-					$content .= '</form>';
-				}
-					// The update button was clicked, perform the update
-				else {
-					while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-						$fields = $row;
-						$fields['sorting'] = 1;
-							// Create new relationship
-						$GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_displaycontroller_providers2_mm', $fields);
-							// Set relationship counter in tt_content
-						$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', "uid = '".$row['uid_local']."'", array('tx_displaycontroller_provider2' => 1));
-						$content .= '<p>New relationship created:</p>';
-						$content .= t3lib_div::view_array($fields);
-					}
-						// Delete old relationships
-					$GLOBALS['TYPO3_DB']->exec_DELETEquery('tx_displaycontroller_providers_mm', "sorting = '2'");
-					$content .= '<p>Old relationships deleted<br /></p>';
-					$content .= '<p><strong>Update complete!</strong></p>';
-				}
-			}
-		}
-		else {
-			$content .= '<p><strong>ERROR: Could not get a list of existing secondary providers. Check if anything is wrong with your database and try again.</strong></p>';
-		}
-		$content .= '<h2>Checking old hook names</h2>';
-		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['displaycontroller']['setExtraDataForFilter']) && count($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['displaycontroller']['setExtraDataForFilter']) > 0) {
-			$content .= '<p>There are some hooks that you need to change <em>(&quot;setExtraDataForFilter&quot; =&gt; &quot;setExtraDataForParser&quot;)</em></p>';
-			$content .= '<ul>';
-			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['displaycontroller']['setExtraDataForFilter'] as $hook) {
-				$content .= '<li>'.$hook.'</li>';
-			}
-			$content .= '</ul>';
-		}
-		else {
-			$content .= '<p>There are no old hooks to change.</p>';
-		}
- *
- */
 		return $content;
 	}
 
