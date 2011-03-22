fd81a7302238bfc61bd3e191ba36843ffb2b19d8
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-22 16:29:09 +0000

    Released to TER as version 1.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dataquery/trunk@45464 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "dataquery".
 #
-# Auto generated 28-12-2010 16:42
+# Auto generated 22-03-2011 17:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,12 +29,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'tesseract' => '1.0.0-0.0.0',
 			'datafilter' => '1.0.0-0.0.0',
-			'overlays' => '1.0.0-0.0.0',
+			'overlays' => '2.0.0-0.0.0',
 			'typo3' => '4.3.0-0.0.0',
 			'expressions' => '',
 		),
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'cachecleaner' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"1a16";s:10:"README.txt";s:4:"b3a6";s:27:"class.tx_dataquery_ajax.php";s:4:"d756";s:28:"class.tx_dataquery_cache.php";s:4:"1106";s:29:"class.tx_dataquery_parser.php";s:4:"9a2b";s:34:"class.tx_dataquery_queryobject.php";s:4:"662c";s:32:"class.tx_dataquery_sqlparser.php";s:4:"e973";s:30:"class.tx_dataquery_wrapper.php";s:4:"64e3";s:16:"ext_autoload.php";s:4:"6a06";s:21:"ext_conf_template.txt";s:4:"6c35";s:12:"ext_icon.gif";s:4:"ebf0";s:17:"ext_localconf.php";s:4:"41bb";s:14:"ext_tables.php";s:4:"3162";s:14:"ext_tables.sql";s:4:"c88c";s:13:"locallang.xml";s:4:"0ff9";s:37:"locallang_csh_txdatafilterfilters.xml";s:4:"73bc";s:36:"locallang_csh_txdataqueryqueries.xml";s:4:"795c";s:16:"locallang_db.xml";s:4:"6374";s:7:"tca.php";s:4:"d635";s:14:"doc/manual.pdf";s:4:"5a7d";s:14:"doc/manual.sxw";s:4:"1f10";s:43:"hooks/class.tx_dataquery_datafilterhook.php";s:4:"5edc";s:34:"res/icons/add_dataquery_wizard.gif";s:4:"909a";s:39:"res/icons/icon_tx_dataquery_queries.gif";s:4:"ebf0";s:22:"res/js/check_wizard.js";s:4:"b347";s:42:"samples/class.tx_dataquery_sample_hook.php";s:4:"6530";s:38:"tests/tx_dataquery_sqlbuilder_Test.php";s:4:"8f9f";s:37:"tests/tx_dataquery_sqlparser_Test.php";s:4:"1d40";s:44:"wizards/class.tx_dataquery_wizards_check.php";s:4:"9b01";}',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"42d7";s:10:"README.txt";s:4:"b3a6";s:27:"class.tx_dataquery_ajax.php";s:4:"d756";s:28:"class.tx_dataquery_cache.php";s:4:"1106";s:29:"class.tx_dataquery_parser.php";s:4:"03f7";s:34:"class.tx_dataquery_queryobject.php";s:4:"b13b";s:32:"class.tx_dataquery_sqlparser.php";s:4:"95c4";s:30:"class.tx_dataquery_wrapper.php";s:4:"f20b";s:16:"ext_autoload.php";s:4:"6a06";s:21:"ext_conf_template.txt";s:4:"6c35";s:12:"ext_icon.gif";s:4:"ebf0";s:17:"ext_localconf.php";s:4:"41bb";s:14:"ext_tables.php";s:4:"3162";s:14:"ext_tables.sql";s:4:"0871";s:13:"locallang.xml";s:4:"0ff9";s:37:"locallang_csh_txdatafilterfilters.xml";s:4:"73bc";s:36:"locallang_csh_txdataqueryqueries.xml";s:4:"6865";s:16:"locallang_db.xml";s:4:"4ba3";s:7:"tca.php";s:4:"0be2";s:14:"doc/manual.pdf";s:4:"1aa2";s:14:"doc/manual.sxw";s:4:"eabc";s:43:"hooks/class.tx_dataquery_datafilterhook.php";s:4:"5edc";s:34:"res/icons/add_dataquery_wizard.gif";s:4:"909a";s:39:"res/icons/icon_tx_dataquery_queries.gif";s:4:"ebf0";s:22:"res/js/check_wizard.js";s:4:"b347";s:42:"samples/class.tx_dataquery_sample_hook.php";s:4:"6530";s:38:"tests/tx_dataquery_sqlbuilder_Test.php";s:4:"5475";s:46:"tests/tx_dataquery_sqlbuilder_default_Test.php";s:4:"4766";s:47:"tests/tx_dataquery_sqlbuilder_language_Test.php";s:4:"b6e8";s:48:"tests/tx_dataquery_sqlbuilder_workspace_Test.php";s:4:"39d6";s:37:"tests/tx_dataquery_sqlparser_Test.php";s:4:"1d40";s:44:"wizards/class.tx_dataquery_wizards_check.php";s:4:"4a40";}',
 	'suggests' => array(
 	),
 );
